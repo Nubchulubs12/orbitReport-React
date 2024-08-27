b8f12598087bd71dc8067c4d44bec298f3408e49
Nubchulubs12 <nick.kuczka@gmail.com>
2024-08-27 18:14:31 -0500

    last done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
+import { useState } from "react";
+import "./App.css";
+import Banner from "./components/Banner";
 import Buttons from "./components/Buttons";
+import satData from "./components/satData";
 import Table from "./components/Table";
+import logo from "/logo192.png";
 
 
 function App() {
+  const [sat, setSat] = useState(satData);
+  const orbitTypes = [...new Set(satData.map((data) => data.orbitType))];
+
+  const filterByType = (currentType) => {
+    const displaySats = satData.filter((newSatDisplay) => {
+      return newSatDisplay.orbitType === currentType;
+    });
+    setSat(displaySats);
+  };
+
+
   return (
     <div>
-      <Buttons />
-      <Table />
+      <Banner />
+      <Buttons filterByType={filterByType}
+        setSat={setSat}
+        orbitTypes={orbitTypes} />
+      <Table sat={sat} />
+      <div className="container-div">
+        <img src={logo} alt="logo" className="App-logo" />
+      </div>
     </div>
   );
 }
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,26 @@
-const Buttons = ({ filterByType, setSat, displaySats }) => {
+import React from "react";
+import satData from "./satData";
+import "./styling.css";
+
+
+const Buttons = ({ filterByType, setSat, orbitTypes }) => {
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+    <div className="flex-container">
+      
+      {orbitTypes.map((type, index) => (
+        <div key={index}>
+          <button onClick={() => filterByType(type)} >
+            {type} Orbit
+          </button>
+          
+        </div>
+      ))}
+
+      <div>
+        <button onClick={() => setSat(satData)}>
+          All orbits
+        </button>
+      </div>
     </div>
   );
 };
